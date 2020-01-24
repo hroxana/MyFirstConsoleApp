5bbaaedf62681446901ebdde63d46956fbcf3596
hroxana <60153128+hroxana@users.noreply.github.com>
2020-01-24 17:30:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFristConsoleAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");//prints a text to trhe console
+            Console.ReadLine();//holds the console open until a key is pressed
+
+        }
+    }
+}
